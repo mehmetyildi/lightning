6e4422a0cb5d3b4d9a4706d81aca6b16a8fbbbf8
Mehmet Yıldırım <mehmetyildi@msn.com>
2018-05-07 21:31:30 +0300

    Git workflow rollback

--- resources/views/layouts/user/header.blade.php
+++ resources/views/layouts/user/header.blade.php
@@ -48,7 +48,6 @@
         <!--Logo-Header-->
         <div class="logoheader logo-top">
             <div class="logo">
-                Lorem
                 <a href="/"><img src="{{$hp->logo}}" style="width:160px; height: 90px;"  alt="image" /></a>
             </div>
         </div>
